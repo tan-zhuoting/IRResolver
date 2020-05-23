c5a4b66e392d53661170b94dc9b7be868ca5d336
Zhuoting Tan <zhuoting@ucla.edu>
2020-05-23 16:12:05 -0700

    Started graphical design of the window and widget

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
-#include <iostream>
-#include <fstream>
+#include "mainwindow.h"
+#include <QApplication>
 
-#include "IRResolver.hpp"
-
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-    IRResolver irr;
-    std::ifstream ifs(argv[1]);
-    std::string line;
-    while (std::getline(ifs, line)) {
-        irr.add_record(line);
-    }
-    ifs.close();
-    while (true) {
-        std::cout << "Please input frequency (0 to quit)\n";
-        unsigned n;
-        std::cin >> n;
-        if (n == 0) {
-            break;
-        }
-        auto recs = irr.find_records(n);
-        for (auto& r : recs) {
-            std::cout << *r << std::endl;
-        }
-    }
-
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <string>
+#include <memory>
+#include "IRResolver.hpp"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_spinBox_valueChanged(int arg1);
+
+    void on_actionLoad_triggered();
 
 private:
     Ui::MainWindow *ui;
+
+    std::shared_ptr<IRResolver> irr;
 };
+
 #endif // MAINWINDOW_H
